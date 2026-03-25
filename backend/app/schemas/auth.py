d5dddb941dd559b225ae400fa180ed12349fc5f5
weedguet_mildort <weedguet.mildort@gmail.com>
2026-03-24 22:26:35 -0400

    Signup form updates

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,9 +1,13 @@
+# backend/app/schemas/auth.py
 from pydantic import BaseModel, EmailStr, Field
 from .user import UserPublic
 
 class SignupRequest(BaseModel):
+    first_name: str = Field(min_length=1)
+    last_name: str = Field(min_length=1)
     email: EmailStr
     password: str = Field(min_length=6)
+    consent: bool
 
 class LoginRequest(BaseModel):
     email: EmailStr
